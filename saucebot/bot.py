50aa8ef0b9592a9130bc8d70e3c06a00fe77aa33
S0nic014 <47799401+S0nic014@users.noreply.github.com>
2021-09-22 21:00:10 +0100

    Functioning sauce search

--- saucebot/bot.py
+++ saucebot/bot.py
@@ -1,6 +1,35 @@
-import discord
+import os
+import pathlib
+from discord.ext import commands
+from dotenv import load_dotenv
 from saucebot.logger import Logger
 
 
-class Bot:
-    pass
+COGS_DIR = pathlib.Path.cwd() / 'saucebot' / 'cogs'
+COMMAND_PREFIX = '?'
+
+
+def list_cogs() -> list:
+    cogs = []
+    for cog_file in COGS_DIR.glob('*.py'):
+        fname = cog_file.name.removesuffix('.py')
+        cogs.append(f'cogs.{fname}')
+    return cogs
+
+
+def main():
+    Logger.write_to_rotating_file('bot.log')
+    try:
+        bot = commands.Bot(COMMAND_PREFIX)
+        for ext in list_cogs():
+            bot.load_extension(ext)
+            Logger.info(f'Loaded extension: {ext}')
+
+        load_dotenv()
+        bot.run(os.getenv('TOKEN'))
+    except Exception:
+        Logger.exception('Unhandled exception')
+
+
+if __name__ == '__main__':
+    main()
